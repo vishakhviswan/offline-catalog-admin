dc2fcbc7d1c08b92ed999611521c57e69c0f5c48
vishakhviswan <vishakhviswan1995@gmail.com>
2026-02-06 02:01:01 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "offline-catalog-admin-v1";
+const ASSETS = [
+  "/offline-catalog-admin/",
+  "/offline-catalog-admin/index.html",
+  "/offline-catalog-admin/manifest.json",
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }),
+  );
 });
 
-self.addEventListener("activate", () => {
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        }),
+      ),
+    ),
+  );
   self.clients.claim();
 });
 
+// FETCH
 self.addEventListener("fetch", (event) => {
-  // network first – no risk for admin app
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    }),
+  );
 });
